54c9957c759c4281e60417251fb67a36d7d8f3bb
amaxilat <d.amaxilatis@gmail.com>
2023-10-23 11:19:28 +0300

    update appSensors.js to use custom backoffs

--- client/appSensors.js
+++ client/appSensors.js
@@ -4,7 +4,11 @@ const http = require('http');
 var Web3 = require('web3');
 const configuration = require('../build/contracts/Sensors.json');
 // const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
-const web3 = new Web3(new Web3.providers.HttpProvider("http://block.amaxilatis.com:8545"));
+const OPTIONS = {
+  defaultBlock: "latest",
+  transactionBlockTimeout: 150
+};
+const web3 = new Web3(new Web3.providers.HttpProvider("http://block.amaxilatis.com:8545"), null, OPTIONS);
 const contract = new web3.eth.Contract(configuration.abi, configuration.networks[5777].address);
 const fs = require('fs');
 const bodyParser = require('body-parser');
@@ -29,26 +33,27 @@ const timeout = 1000;
   const myAccount = accountsList[myIndex];
   console.log(myAccount);
 
-  const tempInterval = setInterval(async () => {
-    if (cnt1 > 99) {
-      clearInterval(tempInterval);
-      return;
-    }
+  async function addTemperature(){
     let theTimeT = await addTemperatureValue(myIndex, myAccount);
     // times.push(theTimeT);
     console.log(theTimeT + " " + cnt1);
     cnt1 += 1;
-  }, timeout);
-
-  const humInterval = setInterval(async () => {
-    if (cnt2 > 99) {
-      clearInterval(humInterval);
-      return;
+    if (cnt1 < 100) {
+      setTimeout(addTemperature, theTimeT)
     }
+  }
+
+  async function addHumidity(){
     let theTimeH = await addHumidityValue(myIndex, myAccount);
     // times.push(theTimeH);
     console.log(theTimeH + " " + cnt2);
     cnt2 += 1;
-  }, timeout);
+    if (cnt2 < 100) {
+      setTimeout(addHumidity, theTimeH)
+    }
+  }
+
+  addTemperature();
+  addHumidity();
 
 })();
